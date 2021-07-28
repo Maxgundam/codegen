e84af56b4cf2bcdfd859689ce079f8d8e4d22d84
Maxgundam <78227585+Maxgundam@users.noreply.github.com>
2021-07-28 17:56:09 -0400

    Update codegen.c

--- codegen.c
+++ codegen.c
@@ -1,5 +1,5 @@
 /*
-	Author: Michael Tsang Jack Eason
+	Author: Michael Tsang, Jack Eason
 */
 
 #include <stdlib.h>
@@ -9,32 +9,210 @@
 
 instruction *code;
 int code_index;
+int tok_index;
+int sym_index;
+int proc_index;
 
 void printcode();
 void emit(int op, int L, int M);
+void expression();
+void PROGRAM(lexeme *tokens, symbol *symbols);
+void BLOCK(int lex_level,lexeme *tokens, symbol *symbols);
+void STATEMENT(int lex_leve, lexeme *tokens, symbol *symbols);
 
 instruction *generate_code(lexeme *tokens, symbol *symbols)
 {
 	code = malloc(500 * sizeof(instruction));
 	code_index = 0;
- 
+  tok_index = 0;
+  sym_index = 0;
+  
+  
   /*
-  code_index = code_index + 1;
-  code[0].opcode = 2;
-  code[0].m = 1;
-  code[0].l = 1;
-  printf("Hey Hey %d\n", code[0].opcode);
-  */
+  for(int i = 0; i < sizeof(symbols); i++)
+  {
+    printf("%d\n", i);
+    printf("Symbol kind %d\n", symbols[i].kind);
+    printf("Symbol name %s\n", symbols[i].name);
+    printf("Symbol value %d\n", symbols[i].val);
+    printf("Symbol level %d\n", symbols[i].level);
+    printf("Symbol addr %d\n", symbols[i].addr);
+    printf("Symbol mark %d\n\n", symbols[i].mark);
   
+  }*/
   
- 
+  PROGRAM(tokens, symbols);
+  
 	printcode();
 	return code;
 }
 
+
+void PROGRAM(lexeme *tokens, symbol *symbols)
+{
+  //Unmark Main
+  symbols[sym_index].mark = 0;
+  sym_index++;
+  
+  emit (7, 0, 0);
+  BLOCK(0,tokens, symbols);
+  emit (9, 0, 3);
+  code[0].m= symbols[0].val * 3;
+
+}
+
+void BLOCK(int lex_level, lexeme *tokens, symbol *symbols)
+{
+  int proc_index = sym_index - 1;
+  int numSym = 0;
+  int numVars = 0;
+  
+  //ENDLESSLY LOOPING: MUST FIX!!!! DELETE THIS COMMENT LATER.
+  printf("Jo: %d\n", proc_index);
+  
+  //CONST_DECLARATION(tokens, symbols)
+  if(tokens[tok_index].type == constsym)
+  {
+    do
+    {
+      tok_index++;
+      numSym++;
+      
+      symbols[sym_index].mark = 0;
+      
+      sym_index++;
+      tok_index = tok_index + 3;
+    
+    
+    }while (tokens[tok_index].type == commasym);
+     
+     tok_index = tok_index + 1;
+  
+  }
+  
+  //VAR_DECLARATION
+  if(tokens[tok_index].type == varsym)
+  {
+    do
+    {
+      tok_index++;
+      numVars++;
+      numSym++;
+      
+      //Unmark variable
+      symbols[sym_index].mark = 0;
+      sym_index++;
+      
+      tok_index++;
+      
+    
+    }while (tokens[tok_index].type == commasym);
+    
+    tok_index++;
+  
+  
+  }
+  
+  //PROCEDURE_DECLARATION
+  if(tokens[tok_index].type == procsym)
+  {
+    do
+    {
+      tok_index++;
+      numSym++;
+      
+      //Unmark procedure
+      symbols[sym_index].mark = 0;
+      sym_index++;
+      
+      BLOCK(lex_level + 1, tokens, symbols);
+      
+      emit(2, 0, 0);
+      
+      tok_index++;
+    
+    }while (tokens[tok_index].type == procsym);
+  
+  }
+  
+  //printf("The code index is: %d\n", code_index);
+  //printf("the proc_index is: %d\n", proc_index);
+  
+  code[proc_index].m = code_index;
+  
+  //printf("%d\n", code[0].m)
+  
+  emit(6, 0, 3 + numVars);
+  STATEMENT(lex_level, tokens, symbols);
+  
+
+}
+
+void STATEMENT(int lex_level, lexeme *tokens, symbol *symbols)
+{
+  if(tokens[tok_index].type == identsym)
+  {
+   
+    
+  
+  }
+  
+  
+  else if(tokens[tok_index].type == callsym)
+  {
+  
+  
+  }
+  
+  
+  else if(tokens[tok_index].type == beginsym)
+  {
+  
+  
+  
+  }
+  
+  
+  else if(tokens[tok_index].type == writesym)
+  {
+  
+  
+  }
+  
+  
+  else if(tokens[tok_index].type == readsym)
+  {
+  
+  
+  }
+  
+  else if(tokens[tok_index].type == ifsym)
+  {
+  
+  
+  }
+  
+  else if(tokens[tok_index].type == whilesym)
+  {
+  
+  
+  
+  }
+
+
+}
+
+
+
+
 void emit(int op, int L, int M)
 {
-  code[code_index].op = op;
+  if(code_index > 500)
+  {
+    printf("error EDIT ME!\n");
+  }  
+  
+  code[code_index].opcode = op;
   code[code_index].l = L;
   code[code_index].m = M;
   code_index++;
